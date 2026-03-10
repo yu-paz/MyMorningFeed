dd602e338be1f04d0b14c13c8fb4ce920d096e68
yu-paz <yuvalpaz2005@gmail.com>
2026-03-09 21:56:05 -0400

    setting up API keys and weather.py

---
+++ weather.py
@@ -0,0 +1,16 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_weather(lat= "40.7128", lon= "-74.0060"):
+    api_key = os.getenv("WEATHER_API_KEY")
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    print(data)
+
+get_weather()
